bc193d355d7a9580c9030b0bffa586fbbfa6750a
Zack Fullerton <zachary.e.fullerton@gmail.com>
2016-02-12 12:17:14 -0600

    updated foreign key references

--- server/schema.js
+++ server/schema.js
@@ -5,15 +5,15 @@ pg.schema.createTableIfNotExists('trip', function (table) {
 pg.schema.createTableIfNotExists('user', function (table) {
 	table.increments('id').primary();
 	table.string('name');
-	table.string('email');
+	table.string('email').unique();
 	table.string('nickname');
 	table.string('password');
 });
 
 pg.schema.createTableIfNotExists('trip_user', function (table) {
 	table.increments('id').primary();
-	table.integer('id_trip').references(trip.id);
-	table.integer('id_user').references(user.id);
+	table.integer('id_trip').references('id').inTable('trip');
+	table.integer('id_user').references('id').inTable('user');
 });
 
 pg.schema.createTableIfNotExists('task', function (table) {
@@ -21,15 +21,15 @@ pg.schema.createTableIfNotExists('task', function (table) {
 	table.string('name');
 	table.string('status');
 	table.string('decision');
-	table.integer('id_trip').references(trip.id);
+	table.integer('id_trip').references('id').inTable('trip');
 });
 
 pg.schema.createTableIfNotExists('message', function (table) {
 	table.increments('id').primary();
 	table.timestamp('createdAt');
 	table.string('content', 1000);
-	table.integer('id_task').references(task.id);
-	table.integer('id_user').references(user.id);
+	table.integer('id_task').references('id').inTable('task');
+	table.integer('id_user').references('id').inTable('user');
 });
 
 pg.schema.createTableIfNotExists('', function (table) {
@@ -37,8 +37,8 @@ pg.schema.createTableIfNotExists('', function (table) {
 	table.string('suggestion');
 	table.integer('votes');
 	table.timestamp();
-	table.integer('id_task').references(task.id);
-	table.integer('id_user').references(user.id);
-	table.integer('id_trip').references(trip.id);
+	table.integer('id_task').references('id').inTable('task');
+	table.integer('id_user').references('id').inTable('user');
+	table.integer('id_trip').references('id').inTable('trip');
 });
 
