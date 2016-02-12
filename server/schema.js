15f09126f83a5ffca981a404f974e0ed01071265
Zack Fullerton <zachary.e.fullerton@gmail.com>
2016-02-12 13:18:23 -0600

    Addressed comments from Jon

--- server/schema.js
+++ server/schema.js
@@ -4,10 +4,11 @@ pg.schema.createTableIfNotExists('trip', function (table) {
 
 pg.schema.createTableIfNotExists('user', function (table) {
 	table.increments('id').primary();
-	table.string('name');
-	table.string('email').unique();
-	table.string('nickname');
-	table.string('password');
+	table.string('name').notNullable();
+	table.string('email').unique().notNullable();
+	table.string('nickname').notNullable();
+	//will replace with password hash
+	table.string('password').notNullable();
 });
 
 pg.schema.createTableIfNotExists('trip_user', function (table) {
@@ -18,29 +19,29 @@ pg.schema.createTableIfNotExists('trip_user', function (table) {
 
 pg.schema.createTableIfNotExists('task', function (table) {
 	table.increments('id').primary();
-	table.string('name');
-	table.string('status');
-	table.string('decision');
+	table.string('name').notNullable();
+	//will set default after decision on wording is made
+	table.string('status').defaultTo('');
+	table.string('decision').nullable();
 	table.integer('id_trip').references('id').inTable('trip');
 });
 
 pg.schema.createTableIfNotExists('message', function (table) {
 	table.increments('id').primary();
-	table.timestamp('createdAt');
-	table.string('content', 1000);
+	table.timestamp('createdAt').defaultTo(pg.fn.now());
+	table.string('content', 1000).notNullable();
 	table.integer('id_task').references('id').inTable('task');
 	table.integer('id_user').references('id').inTable('user');
 });
 
-pg.schema.createTableIfNotExists('', function (table) {
+//will replace default with correct table name after decision on wording is made
+pg.schema.createTableIfNotExists('default', function (table) {
 	table.increments('id').primary();
-	table.string('suggestion');
-	table.integer('votes');
-	table.timestamp();
+	table.string('suggestion').notNullable();
+	table.integer('votes').defaultTo(0);
+	table.timestamp('createdAt')defaultTo(pg.fn.now());
 	table.integer('id_task').references('id').inTable('task');
 	table.integer('id_user').references('id').inTable('user');
 	table.integer('id_trip').references('id').inTable('trip');
 });
-
-
 
