88e5a59989a1fc5721f2f286bcd612e4b085c7f1
MKS-students <mks.students@hackreactor.com>
2016-02-15 13:18:53 -0600

    Pushing to repo because I have to relocate from student workstation! fun progress, though

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const env    = process.env.NODE_ENV || 'development';
 const pg     = require('knex')(config[env]);
 
 // This helps to ensure that the running database's schema is up to date
-pg.migrate.latest();
+// pg.migrate.latest();
 
 
 module.exports = pg;
