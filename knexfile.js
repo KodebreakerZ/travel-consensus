9aa875c8e5333fecd819cd4a09b87820c039f3ec
MKS-students <mks.students@hackreactor.com>
2016-02-15 14:39:53 -0600

    Update knexfile documentation to include information about seeding the database

--- knexfile.js
+++ knexfile.js
@@ -1,35 +1,33 @@
 /*
   DATABASE CONFIGURATION
 
-    Postico is a fantastic postgresql database visualizer
-    'psql db/' is a great tool for command-line db access.
+    Postico is a fantastic postgresql database visualizer!
+    'psql db/' is a great tool for command-line db access!
 
-  Hi! To get a brand-new development database up and running:
-    > navigate to project directory
-    > ensure PostGreSql is installed
-      > run postgres in your terminal and see if you have anything
-        > if not, 'brew install postgres'
-    > run 'initdb db/'
-    > run 'createdb db/dev'
-    > A database server must be up and running to test and develop!
-      > open a new terminal, make sure you're in the project root directory
-      > run 'postgres -D db/'
+    Hi! To get a brand-new development database up and running:
+      > navigate to project directory
+      > ensure PostGreSql is installed
+        > run 'postgres' in your terminal and see if you have anything
+          > if not, 'brew install postgres'
+      > 'initdb db/' to initialize a database system
+      > 'createdb development' to create a development database
+      > A database server must be up and running to test and develop!
+        > open a new terminal, make sure you're in the project root directory
+        > 'postgres -D db/' to start the database server
 
-    The database should be up and grump-ily accepting connections
-    go ahead and run npm start/test to get a server or test running.
+      The database should be up and grump-ily accepting connections
+      go ahead and run npm start/test to get a server or test running.
 
   ************************************
 
-    UPDATING DATABASE SCHEMA
+  DATABASE TEST SEEDING
 
-    The 'migrations' folder helps us to keep track of database schema changes
-    and will simplify updates as our postgresql becomes more complex and we have
-    a longer version history.
-    When making changes to the database schema:
-      > run 'npm install -g knex' to access knex CLI
-      > run 'knex migrate:make consensus' to create a new, timestamped schema file
-      > Copy the previous schema and paste it into the new migration file
-      > Make any changes!
+    To seed the database with data so that model operations can be tested
+    run the above database setup with the following additions:
+      > 'npm install -g knex' to access knex on the command line
+      > 'postgres -D db/' to start database server
+      > 'createdb test' to create a test database
+      > 'knex --env test seed:run' to seed the database with data
 */
 
 module.exports = {
