3b83fc2c457422c7c5c5a59e31b130aaefcb8caa
MKS-students <mks.students@hackreactor.com>
2016-02-17 14:51:23 -0600

    (feat) write task api endpoints. This opened a can of things to do: sort messages and suggestions chronologically; write suggestion model; refactor trip api to be more consistent with design style of the task api; implement database updating for tasks; removal of suggestions.

--- server/apis/task-api.js
+++ server/apis/task-api.js
@@ -2,49 +2,94 @@ require('../server-helper');
 
 var TaskAPI = require('express').Router();
 var Task    = require('../../models/task');
+var Message = require('../../models/message');
+var Suggestion = require('../../models/suggestion');
 
 module.exports = TaskAPI;
 
 
 /*
-  POST task/
+  POST task/:id_task/message
 
-  Creates a new task from the name and tripId of the request
+  Creates a new message for this task
 
   Expects request: {
-    name:   String,
-    tripId: Number
+    content: String <What was said>
+    id_user:  Number <Who said it>
   }
 
-  Responds with the new task: {
-    id:       Number,
-    name:     String,
-    status:   String,
-    decision: String,
-    id_trip:  Number
+  Responds with the new message: {
+    id:        Number <unique identifier for this message>
+    content:   String <What was said>
+    createdAt: Date <When it was said>
+    id_user:   Number <Who said it>
+    id_task:   Number <In which task>
   }
 */
-TaskAPI.post('/', function(request, response) {
-  var newTask = {
-    name:    request.body.name,
-    id_trip: request.body.tripId
+TaskAPI.post('/:id_task/message', function(request, response) {
+  var newMessage = {
+    content: request.body.content,
+    id_user: request.body.id_user,
+    id_task: request.params.id_task
   }
 
-  Task.create(newTask)
-    .then(function(newTask) {
-      response.send(trip)
+  Message.create(newTask)
+    .then(function(newMessage) {
+      response.send(newMessage)
     })
     .catch(function(error) {
       console.error('ERROR POST:', request.url);
-      response.status(500).send('Server error posting new task');
+      response.status(500).send('Server error posting new message to task');
     })
 })
 
+/*
+  MODEL NOT IMPLEMENTED
+
+  POST task/:id_task/suggestion
+
+  Creates a new suggestion for this task
+
+  Expects request: {
+    suggestion: String <What was suggested>
+    id_user:    Number <Who suggested it>
+  }
+
+  Responds with the new suggestion: {
+    id:         Number <unique identifier for this message>
+    suggestion: String <What was suggested>
+    createdAt:  Date <When it was suggested>
+    id_user:    Number <Who suggested it>
+    id_task:    Number <In which task>
+  }
+*/
+TaskAPI.post('/:id_task/message', function(request, response) {
+  var newSuggestion = {
+    suggestion: request.body.suggestion,
+    id_user: request.body.id_user,
+    id_task: request.params.id_task
+  }
+
+  Suggestion.create(newSuggestion)
+    .then(function(newSuggestion) {
+      response.send(newSuggestion)
+    })
+    .catch(function(error) {
+      console.error('ERROR POST:', request.url);
+      response.status(500).send('Server error posting new suggestion to task');
+    })
+})
 
 /*
-  GET task/:taskId
+  NOT IMPLEMENTED
 
-  Returns the task with matching taskId
+  PUT task/:id_task
+
+  Updates a task's status
+
+  Expects request: {
+    // Do we need full information or can we update only given properties
+  }
 
   Responds with: {
     id:       Number,
@@ -54,19 +99,70 @@ TaskAPI.post('/', function(request, response) {
     id_trip:  Number
   }
 */
-TaskAPI.get('/:taskId', function(request, response) {
-  var taskId = request.params.taskId;
+TaskAPI.put('/:id_task/', function(request, response) {
 
-  Task.byId(taskId)
-    .then(function(task) {
-      response.send(task);
+})
+
+/*
+  TODO: Sort messages in chronological order in message model.
+
+  GET task/:id_task/messages
+
+  Returns all messages of a given task
+
+  Responds with: [
+    {
+      id:        Number <unique identifier for this message>
+      content:   String <What was said>
+      createdAt: Date <When it was said>
+      id_user:   Number <Who said it>
+      id_task:   Number <In which task>
+    }
+  ]
+*/
+TaskAPI.get('/:id_task/messages', function(request, response) {
+  var id_task = request.params.id_task;
+
+  Message.allOfTask(id_task)
+    .then(function(allMessages) {
+      response.send(allMessages);
     })
     .catch(function(error) {
       console.error('ERROR GET:', request.url);
-      response.status(500).send('Server error getting task by id');
+      response.status(500).send('Server error getting messages by task id');
     })
 })
 
+
+/*
+  TODO: Sort suggestions in chronological order in suggestion model.
+
+  GET task/:id_task/suggestions
+
+  Returns all suggestions of a given task
+
+  Responds with: [
+    {
+      id:         Number <unique identifier for this message>
+      suggestion: String <What was suggested>
+      createdAt:  Date <When it was suggested>
+      id_user:    Number <Who suggested it>
+      id_task:    Number <In which task>
+    }
+  ]
+*/
+TaskAPI.get('/:id_task/suggestions', function(request, response) {
+  var id_task = request.params.id_task;
+
+  Suggestion.allOfTask(id_task)
+    .then(function(allSuggestions) {
+      response.send(allSuggestions);
+    })
+    .catch(function(error) {
+      console.error('ERROR GET:', request.url);
+      response.status(500).send('Server error getting suggestions by task id');
+    })
+})
 
 
 
