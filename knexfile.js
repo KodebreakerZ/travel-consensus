bb04b75b498cd0ff683a661c9bee3fe1916fd0d3
MKS-students <mks.students@hackreactor.com>
2016-02-17 17:38:58 -0600

    Alright. Crossing my fingers

--- knexfile.js
+++ knexfile.js
@@ -6,28 +6,48 @@
 
     Hi! To get a brand-new development database up and running:
       > navigate to project directory
+
       > ensure PostGreSql is installed
         > run 'postgres' in your terminal and see if you have anything
           > if not, 'brew install postgres'
+
       > 'initdb db/' to initialize a database system
-      > 'createdb development' to create a development database
-      > A database server must be up and running to test and develop!
+        > This creates a file structure with a root folder of 'db/' in your
+          project directory. This is where postgres stores a whole load of
+          information that represents a database. We have not created a
+          database yet - only a place to put one.
+
+      > To use a postgres database, there are two entities involved: a Server
+        and a Client. Just like HTTP, a server responds to requests from some
+        client. A server processes requests to determine what information to
+        send back. We need to start a database-server for our app-server to
+        interact with.
         > open a new terminal, make sure you're in the project root directory
         > 'postgres -D db/' to start the database server
 
-      The database should be up and grump-ily accepting connections
-      go ahead and run npm start/test to get a server or test running.
+      > Now we have a running database-server, we need to tell it to create
+        the actual database.
+        > 'createdb development' to create a database named development.
+        > This is processed and stored in the db/ folder we made just a couple
+          steps ago.
+
+      The database should be up, empty, and grump-ily accepting connections.
+      If you want to run tests, this should be sufficient. If you want to
+      use it with sample data for development, read the next section.
 
   ************************************
 
-  DATABASE TEST SEEDING
+  DATABASE SEEDING for development
 
     To seed the database with data so that model operations can be tested
-    run the above database setup with the following additions:
+    run the ^^above^^ database setup steps with the following additions:
       > 'npm install -g knex' to access knex on the command line
-      > 'postgres -D db/' to start database server
-      > 'createdb test' to create a test database
-      > 'knex --env test seed:run' to seed the database with data
+      > if you don't have a database server running,
+        > 'postgres -D db/' to start database server
+      > if you don't have a database intialized,
+        > createdb development' to create a database for development
+      > 'knex --env development seed:run' to seed the 'development' database
+        with data munchkins!
 */
 
 module.exports = {
@@ -42,6 +62,9 @@ module.exports = {
       directory: './migrations',
       tableName: 'knex_migrations'
     },
+    seeds: {
+      directory: './lib/seeds'
+    },
     debug: true,
   },
 
@@ -51,9 +74,6 @@ module.exports = {
       host: 'localhost',
       port: 5432,
       database: 'test'
-    },
-    seeds: {
-      directory: './lib/seeds'
     },
     debug: false,
   },
