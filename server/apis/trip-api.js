28c5128ae06a3a8e7d1c343d15186ce68c9b50d6
MKS-students <mks.students@hackreactor.com>
2016-02-17 18:22:48 -0600

    Implement the server-helper! We have a response helper now! Aren't you excited?

--- server/apis/trip-api.js
+++ server/apis/trip-api.js
@@ -1,9 +1,10 @@
-require('../server-helper');
+require('../server-helper'); // Keep me near the top; I load a few global utilities.
 
 var TripAPI = require('express').Router();
-var Trip    = require('../../models/trip');
-var Task    = require('../../models/task');
-var User    = require('../../models/user');
+
+var Trip    = require(__models + '/trip');
+var Task    = require(__models + '/task');
+var User    = require(__models + '/user');
 
 module.exports = TripAPI;
 
@@ -27,9 +28,7 @@ TripAPI.post('/', function(request, response) {
   }
 
   Trip.create(newTrip)
-    .then(function(newTrip) {
-      response.send(newTrip);
-    })
+    .then(sendStatusAndData(response, 201))
     .catch(function(error) {
       console.error('ERROR POST:', request.url);
       response.status(500).send('Server error posting new trip');
@@ -58,9 +57,7 @@ TripAPI.post('/:id_trip/user', function(request, response) {
   }
 
   Trip.addUser(newUserTrip)
-    .then(function(userTrip) {
-      response.send(userTrip);
-    })
+    .then(sendStatusAndData(response, 201))
     .catch(function(error) {
       console.error('ERROR POST:', request.url);
       response.status(500).send('Server error posting new user to trip');
@@ -91,9 +88,7 @@ TripAPI.post('/:id_trip/task', function(request, response) {
   }
 
   Task.create(newTask)
-    .then(function(newTask) {
-      response.send(newTask);
-    })
+    .then(sendStatusAndData(response, 201))
     .catch(function(error) {
       console.error('ERROR POST:', request.url);
       response.status(500).send('Server error posting new user to trip');
@@ -120,9 +115,7 @@ TripAPI.get('/:tripId/tasks', function(request, response) {
   var tripId = request.params.tripId;
 
   Task.allOfTrip(tripId)
-    .then(function(allTasks) {
-      response.send(allTasks);
-    })
+    .then(sendStatusAndData(response, 200))
     .catch(function(error) {
       console.error('ERROR GET:', request.url);
       response.status(500).send('Server error getting tasks by trip id');
@@ -149,9 +142,7 @@ TripAPI.get('/:tripId/users', function(request, response) {
   var tripId = request.params.tripId;
 
   User.allOfTrip(tripId)
-    .then(function(allUsers) {
-      response.send(allUsers);
-    })
+    .then(sendStatusAndData(response, 200))
     .catch(function(error) {
       console.error('ERROR GET:', request.url);
       response.status(500).send('Server error getting users by trip id');
--- server/server-helper.js
+++ server/server-helper.js
@@ -1,5 +1,57 @@
 var ramda = require('ramda');
 
+global.__models = __dirname + '/../models';
+global.__server = __dirname + '/../server';
+
+/*
+    A NOTE ON CURRY
+      -~- it is delicious -~-
+
+  These helpers use what is called currying. A curried function
+  does not need to receive all of its parameters at once. When
+  you use a curried function, you typically pass it one or two
+  of its parameters and let the last one be passed by the program
+  itself.
+  -!- Until the last parameter has been passed, it does not run -!-
+
+  This gives use more flexibility using our functions. For example,
+  we have built an error reporter that you could place anywhere
+  you might see an error, with a description that helps you identify
+  that error log against others.
+
+  Check it, team. If you aren't clear, let me know so I can change
+  the docs.
+*/
+
+/*
+  A curried error logger that logs a description
+  (where the error happened) and the error message.
+
+  When using this helper, provide it a description
+  and the logging will be invoked only when it is passed
+  a second parameter.
+
+  use example:
+    .catch(reportError('messages from task'))
+*/
+global.reportError = ramda.curry( function(description, error) {
+  console.error('*** Testing Error ***');
+  console.error(description);
+  console.error(error);
+
+  if (error instanceof Error) throw error
+})
+
+
+/*
+  A `shortcut` for responding with data.
+
+  Use it when returning the results of a database operation.
+
+  use example:
+    Trip.create(newTrip)
+      .then(sendStatusAndData(response, 201))
+*/
 global.sendStatusAndData = ramda.curry( function(response, status, data) {
   response.status(status).send(data);
 })
