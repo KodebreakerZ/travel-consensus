7a235cf6041ee115e52876c8f359e5a920355ee9
Jonathan <jkrone@vt.edu>
2016-02-20 00:20:36 -0600

    Update server-helper.js

--- server/server-helper.js
+++ server/server-helper.js
@@ -7,36 +7,27 @@ global.__server = __dirname + '/../server';
     A NOTE ON CURRY
       -~- it is delicious -~-
 
-  These helpers use what is called currying. A curried function
-  does not need to receive all of its parameters at once. When
-  you use a curried function, you typically pass it one or two
-  of its parameters and let the last one be passed by the program
-  itself.
-  -!- Until the last parameter has been passed, it does not run -!-
+  A curried function does not need to receive all of its parameters
+  at once. They're useful because you can pass some information
+  manually and then have the program pass in more as it is running.
 
   This gives use more flexibility using our functions. For example,
-  we have built an error reporter that you could place anywhere
-  you might see an error, with a description that helps you identify
-  that error log against others.
-
-  Check it, team. If you aren't clear, let me know so I can change
-  the docs.
+  we have built an error reporter that you can use to eat an error
+  and then spit back out the error and a description that tells you
+  where it came from.
+  
+    -!- they do not run until the last parameter is passed -!-
 */
 
 /*
-  A curried error logger that logs a description
-  (where the error happened) and the error message.
-
-  When using this helper, provide it a description
-  and the logging will be invoked only when it is passed
-  a second parameter.
+  Provide a description to log when it is invoked with an error or
+  psuedo-error
 
   use example:
-    .catch(reportError('messages from task'))
+    .catch(reportError('error finding messages for a task'))
 */
 global.reportError = ramda.curry( function(description, error) {
-  console.error('*** Testing Error ***');
-  console.error(description);
+  console.error('*** ', description, ' ***');
   console.error(error);
 
   if (error instanceof Error) throw error
@@ -51,6 +42,8 @@ global.reportError = ramda.curry( function(description, error) {
   use example:
     Trip.create(newTrip)
       .then(sendStatusAndData(response, 201))
+    is the same as
+      .then(function(data) { sendStatusAndData(response, 201, data) }) 
 */
 global.sendStatusAndData = ramda.curry( function(response, status, data) {
   response.status(status).send(data);
