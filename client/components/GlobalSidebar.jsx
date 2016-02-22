8194d36d6620137677875a818d365b612836a893
Kamran Varahramyan <kamran@Kamrans-MacBook.local>
2016-02-22 11:08:23 -0600

    temperature is displayed after ajax request

--- client/components/GlobalSidebar.jsx
+++ client/components/GlobalSidebar.jsx
@@ -7,8 +7,23 @@ var TaskItem = require('./TaskItem.jsx');
 GlobalSidebar = React.createClass({
   getInitialState: function() {
     return {
-      tasksInList: []
-    }
+      tasksInList: [],
+      city: "Las Vegas",
+      temperature: ""
+      }
+  },
+
+  componentDidMount: function() {
+    $.ajax({
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=' + this.state.city + '&appid=ce8b5f7c6fbfcba5e114640942ab7893',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        console.log(data.main.temp);
+        // convert API response from Kelvin to Fahrenheit
+        this.state.temperature = " | " + ((data.main.temp * 9 / 5) - 459.67).toFixed(0) + "°";
+      }.bind(this)
+    })
   },
 
   render: function() {
@@ -17,7 +32,7 @@ GlobalSidebar = React.createClass({
         <div className="twelve columns sidebar-height">
           <div className="location-display">
             <i className="fa fa-circle fa-paper-plane-o fa-5x"></i>
-            <h5>Las Vegas | 62</h5>
+            <h5>{this.state.city} {this.state.temperature}</h5>
           </div>
           <hr />
           <TaskList tasks={this.state.tasksInList} addNewTask={this.props.addNewTask} />
