7e055b92bc7b182a9cd123be28bdf4bcc2730e24
Amberley Romo <amberleyjohanna@gmail.com>
2016-02-22 11:11:59 -0600

    began adding suggestion delete process

--- client/components/SuggestionItem.jsx
+++ client/components/SuggestionItem.jsx
@@ -2,9 +2,15 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 SuggestionItem = React.createClass({
+  clickHandler: function() {
+    console.log('delete suggestion w id: ', this.props.suggestion.suggestion.id);
+
+
+  },
+
   render: function() {
     return (
-      <p className="suggestion">
+      <p className="suggestion" onClick={this.clickHandler}>
       {this.props.suggestion.suggestion} <i className="fa fa-times"></i>
       </p>
     )
