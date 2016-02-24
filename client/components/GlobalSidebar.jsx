487e5d6c8fd899f64c330d27eb1e4982a4ee2c6e
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-02-24 17:55:13 -0600

    deleted extra code

--- client/components/GlobalSidebar.jsx
+++ client/components/GlobalSidebar.jsx
@@ -12,18 +12,6 @@ var GlobalSidebar = React.createClass({
 
   componentDidMount: function() {
     setInterval( this.updateTrip, 2000);
-
-    // console.log('trip list ', TripDropdownList.props);
-    // $.ajax({
-    //   url: 'http://api.openweathermap.org/data/2.5/weather?q=' + this.state.city + '&appid=ce8b5f7c6fbfcba5e114640942ab7893',
-    //   dataType: 'json',
-    //   cache: false,
-    //   success: function(data) {
-    //     console.log(data.main.temp);
-    //     // convert API response from Kelvin to Fahrenheit
-    //     this.state.temperature = " | " + ((data.main.temp * 9 / 5) - 459.67).toFixed(0) + "°";
-    //   }.bind(this)
-    // })
   },
 
   updateTrip: function() {
